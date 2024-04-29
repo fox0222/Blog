fa57458be13625c3935bb62d12ac5ff2c4eeda10
fox0222 <92821351+fox0222@users.noreply.github.com>
2024-04-29 10:45:59 +0300

    Set environment variables

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from hashlib import md5
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -27,7 +28,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ["FLASK_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -37,7 +38,7 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -264,4 +265,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
